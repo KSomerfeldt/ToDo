7e3fdaafd5f6eee6c64322c0f5e9cb80f9353f40
Kyle Somerfeldt <kyle.somerfeldt@gmail.com>
2018-11-03 13:24:11 -0400

    Add files via upload

---
+++ assets/js/todos.js
@@ -0,0 +1,22 @@
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("strike");
+});
+
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		var entry = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + entry + "</li>")
+	}
+});
+
+$("#plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
